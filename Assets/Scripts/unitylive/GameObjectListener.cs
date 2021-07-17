03ebac1ad55316ba9dace0308011ab3b35878db0
watanabe@tiffa <watanabe.everystudio@gmail.com>
2021-07-17 16:51:14 +0900

    GameObject用のリスナーとかをテスト追加

---
+++ Assets/Scripts/unitylive/GameObjectListener.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectListener : MonoBehaviour
+{
+    public ParamListener paramListener;
+    void Start()
+    {
+        if (paramListener != null)
+        {
+            paramListener.EventGameObject.AddListener((value) =>
+            {
+                Debug.Log(value.name);
+            });
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/unitylive/ParamListener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+
+public class UnityEventGameObject : UnityEvent<GameObject> { }
+
+public class ParamListener : MonoBehaviour
+{
+    public TextMeshProUGUI m_txt;
+
+    public UnityEventGameObject EventGameObject = new UnityEventGameObject();
+    public Slider slider;
+    void Start()
+    {
+        slider.onValueChanged.AddListener((value) =>
+        {
+            EventGameObject.Invoke(this.gameObject);
+            m_txt.text = ((int)value).ToString();
+        });
+    }
+}
